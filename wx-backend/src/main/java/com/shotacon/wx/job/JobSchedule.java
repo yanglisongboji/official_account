bd91bc52f6d9a8e470530f87d1d672665c0cd838
shotacon <shotacon02@hotmail.com>
2018-10-25 09:28:52 +0800

    Shotacon
    2

--- wx-backend/src/main/java/com/shotacon/wx/job/JobSchedule.java
+++ wx-backend/src/main/java/com/shotacon/wx/job/JobSchedule.java
@@ -1,16 +1,11 @@
 package com.shotacon.wx.job;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.shotacon.wx.config.JobManager;
 import com.shotacon.wx.util.SignatureUtil;
 
-@Component
 public class JobSchedule {
 
-	@Autowired
-	public void init() {
+	static {
 		JobManager.addJob("RefreshAccessToken", "wx", "PostConstruct", "startWithService", RefreshAccessTokenJob.class,
 				"0 0/55 0/1 * * ?");
 		SignatureUtil.reFreshAccessToken();
