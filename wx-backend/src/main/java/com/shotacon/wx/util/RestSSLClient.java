1c606d29811ff4bb108e8b2847aa1797e44a3407
Shotacon <shotacon02@hotmail.com>
2018-11-09 16:18:22 +0800

    1

--- wx-backend/src/main/java/com/shotacon/wx/util/RestSSLClient.java
+++ wx-backend/src/main/java/com/shotacon/wx/util/RestSSLClient.java
@@ -1,10 +1,15 @@
 package com.shotacon.wx.util;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.alibaba.fastjson.JSONObject;
@@ -41,13 +46,22 @@ public class RestSSLClient {
 	 */
 	public static HttpHeaders headersForMultipart;
 
+	private static StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(
+			StandardCharsets.UTF_8);
+
 	static {
 		headers = new HttpHeaders();
 		headers.setContentType(MediaType.parseMediaType("application/json; charset=UTF-8"));
 		headers.add("Accept", MediaType.APPLICATION_JSON.toString());
-		
+
 		headersForMultipart = new HttpHeaders();
 		headersForMultipart.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		// 修改编码
+		httpRestTemplate.setMessageConverters(initHttpMessageConverter(httpRestTemplate.getMessageConverters()));
+		httpsRestTemplate.setMessageConverters(initHttpMessageConverter(httpsRestTemplate.getMessageConverters()));
+		httpsRestTemplateForMultipart
+				.setMessageConverters(initHttpMessageConverter(httpsRestTemplateForMultipart.getMessageConverters()));
 	}
 
 	/**
@@ -59,6 +73,18 @@ public class RestSSLClient {
 	public static HttpEntity<String> entityFromMap(@SuppressWarnings("rawtypes") Map map) {
 
 		return new HttpEntity<>(JSONObject.toJSONString(map), headers);
+	}
+
+	private static List<HttpMessageConverter<?>> initHttpMessageConverter(
+			List<HttpMessageConverter<?>> messageConverters) {
+		List<HttpMessageConverter<?>> resultList = new ArrayList<HttpMessageConverter<?>>();
+		messageConverters.forEach(messageConverter -> {
+			if (messageConverter instanceof StringHttpMessageConverter)
+				resultList.add(stringHttpMessageConverter);
+			else
+				resultList.add(messageConverter);
+		});
+		return resultList;
 	}
 
 }
