6459326356b7af1ba92bfa20468d5056f2140645
Shotacon <shotacon02@hotmail.com>
2019-01-29 15:23:11 +0800

    1

--- wx-backend/src/main/java/com/shotacon/wx/controller/IndexController.java
+++ wx-backend/src/main/java/com/shotacon/wx/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.shotacon.wx.controller;
 
+import java.net.InetAddress;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shotacon.wx.util.IPUtil;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,9 +16,13 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 public class IndexController {
 
-	@ApiOperation(value = "index", notes = "index", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index() {
-		return "What's up, Yo.";
-	}
+    @ApiOperation(value = "index", notes = "index", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() throws Exception {
+        InetAddress address = IPUtil.getLocalHostLANAddress();
+        String hostAddress = address.getHostAddress();
+        System.out.println(hostAddress);
+        return hostAddress + "Nice Try Bitch!";
+    }
+
 }
---
+++ wx-backend/src/main/java/com/shotacon/wx/util/IPUtil.java
@@ -0,0 +1,69 @@
+package com.shotacon.wx.util;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class IPUtil {
+
+    public static InetAddress getLocalHostLANAddress() throws Exception {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<String> getLocalIPList() {
+        List<String> ipList = new ArrayList<String>();
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface networkInterface;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress inetAddress;
+            String ip;
+            while (networkInterfaces.hasMoreElements()) {
+                networkInterface = networkInterfaces.nextElement();
+                inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    inetAddress = inetAddresses.nextElement();
+                    if (inetAddress != null && inetAddress instanceof Inet4Address) { // IPV4
+                        ip = inetAddress.getHostAddress();
+                        ipList.add(ip);
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ipList;
+    }
+}
